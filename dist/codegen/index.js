96b8d1409b9eeada4897abaedab31038ac885902
Yousuf Hossain <yousuf.hossain.shanto@gmail.com>
2022-12-26 23:28:29 +0600

    Fixed

--- dist/codegen/index.js
+++ dist/codegen/index.js
@@ -38,7 +38,7 @@ function codegenContractTypings(contract, codegenConfig) {
         'type @ethersproject/providers': ['Listener', 'Provider'],
     }, source) +
         '\n' +
-        `import { ${contract.name}__factory } from "../index.ts"
+        `import { ${contract.name}__factory } from "../index"
      import { useProvider, useSigner } from "wagmi";
      import { useQuery, useMutation } from "@tanstack/react-query";
     `;
